cad7403400322dc415411dc435a79ee2bdf9e944
Carter J. Bastian <carter.bastian1@gmail.com>
2019-03-04 19:38:41 -0800

    Fixing up submodules

--- deploy.sh
+++ deploy.sh
@@ -3,10 +3,11 @@
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
 # Build the project.
-hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
+hugo -t ghostwriter
 
 # Go To Public folder
 cd public
+
 # Add changes to git.
 git add .
 
@@ -15,7 +16,7 @@ msg="rebuilding site `date`"
 if [ $# -eq 1 ]
   then msg="$1"
 fi
-git commit --allow-empty -m "$msg"
+git commit -m "$msg"
 
 # Push source and build repos.
 git push origin master
